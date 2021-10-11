1d1c74e7a2aa12ee965ce359345384c69906d0d8
Mats15 <matias2.vornanen@gmail.com>
2021-10-11 11:14:40 +0300

    script + css

--- Running-Tracker/scripts.js
+++ Running-Tracker/scripts.js
@@ -1,23 +1,63 @@
+const goal = 25;
+//Kilometer arrray
 let entries = [];
 const entriesWrapper = document.querySelector("#entries");
+document.querySelector('#target').innerText = goal;
 
 function addNewEntry(newEntry) {
     entriesWrapper.removeChild(entriesWrapper.firstElementChild);
     const listItem = document.createElement("li");
-    const listValue = document.createTextNode(newEntry);
+    const listValue = document.createTextNode(newEntry.toFixed(1));
     listItem.appendChild(listValue);
 
     entriesWrapper.appendChild(listItem);
 }
 
+//Total + current value
+function reducer(total, currentValue) {
+    return total + currentValue;
+}
+
+//Calculate total
+function calcTotal() {
+    const totalValue = entries.reduce(reducer);
+    document.getElementById('total').innerText = totalValue.toFixed(1);
+    document.getElementById('progressTotal').innerText = totalValue;
+}
+
+//Average distance calc
+function calcAverage() {
+    const average = (entries.reduce(reducer) / entries.length).toFixed(1);
+    document.getElementById('average').innerText = average;
+}
+//Weekly high calculate
+function weeklyHigh() {
+    const high = Math.max(...entries)
+    document.getElementById('high').innerText = high;
+}
+
+//Progress bar
+function calcGoal() {
+    const totalValue = entries.reduce(reducer).toFixed(1);
+    const completedPercent = totalValue / (goal / 100);
+    const progressCircle = document.querySelector('#progressCircle');
+  
+    if (completedPercent > 100) completedPercent === 100
+    progressCircle.style.background = `conic-gradient(
+      #70db70 ${completedPercent}%, #2d3740 ${completedPercent}% 100%`
+  }
+
 function handleSubmit(event) {
     event.preventDefault();
     const entry = Number(document.querySelector("#entry").value);
     if(!entry) return;
-    document.querySelector('form').reset();
     entries.push(entry);
+    document.querySelector('form').reset();  
     addNewEntry(entry);
-    console.log(entries);
+    calcTotal();
+    calcAverage();
+    weeklyHigh();
+    calcGoal();
 }
 
 const form = document
